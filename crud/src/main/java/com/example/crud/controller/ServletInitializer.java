b05366b64a32023f8e249c99ec29d71986f88d1e
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-02-18 20:27:07 +0000

    Project and dependencies configured.

---
+++ crud/src/main/java/com/example/crud/controller/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.example.crud.controller;
+
+import com.example.crud.CrudApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(CrudApplication.class);
+    }
+}
